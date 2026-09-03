1e6f24913e3f24741a3e10e741a11d0b4fb287cd
Erick Neres <erickppn@gmail.com>
2026-09-03 11:21:46 -0300

    chore: remove loggers from api

--- apps/api/src/modules/auth/auth.service.ts
+++ apps/api/src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import * as argon2 from "argon2";
 import { LoginDto } from "./dto/login.dto";
 import { UsersService } from "../users/users.service";
@@ -6,32 +6,22 @@ import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  private readonly logger = new Logger(AuthService.name)
-
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
   ) { }
 
   async login(data: LoginDto) {
-    this.logger.log(`Login attempt: ${data.email}`);
-
     const user = await this.usersService.findByEmail(data.email);
-
-    this.logger.log(`User found: ${!!user}`);
 
     if (!user) {
       throw new UnauthorizedException("Invalid credentials or email");
     }
 
-    this.logger.log(`Password length: ${data.password} - ${data.password.length}`);
-
     const isPasswordValid = await argon2.verify(
       user.passwordHash,
       data.password,
     );
-
-    this.logger.log(`Password matches: ${isPasswordValid}`);
 
     if (!isPasswordValid) {
       throw new UnauthorizedException("Invalid credentials or email");
